facbdd686db0dd4fd5b7b6d5d87dd8f1131c2a0a
franra9 <francescrouradserias@gmail.com>
2021-01-02 14:19:57 +0100

    some sample script

--- climvis/main.py
+++ climvis/main.py
@@ -1,17 +1,32 @@
 import enso_dwl
 import enso_functions
 
-
-region = "en4"
-filein = 'ERA5_Monthly_sst_2015_'+ region + '.nc'
-smonth = 1
-fmonth = 12
-syear = 1996
-fyear = 2015   
+smonth = 4
+fmonth = 3
+syear = 2005
+fyear = 2010 
+region = "en3"
+filein = 'ERA5_Monthly_sst_' + str(fyear) + '_'+ region + '.nc'
+  
 
 enso_dwl.dwl_era5_enso(fyear, region)
 clim = enso_functions.clim(filein)
-ano = enso_functions.yearly_evol(clim, filein, syear, fyear, smonth, fmonth)
-ano.plot()
+ano1 = enso_functions.yearly_evol(clim, filein, syear, fyear, smonth, fmonth)
+ano1.plot()
 
+smonth = 4
+fmonth = 3
+syear = 2005
+fyear = 2010 
+region = "en12"
+filein = 'ERA5_Monthly_sst_' + str(fyear) + '_'+ region + '.nc'
+  
+
+enso_dwl.dwl_era5_enso(fyear, region)
+clim = enso_functions.clim(filein)
+ano2 = enso_functions.yearly_evol(clim, filein, syear, fyear, smonth, fmonth)
+ano2.plot()
+
+pearson = enso_functions.corr(ano1, ano2)
+print(pearson)
 
